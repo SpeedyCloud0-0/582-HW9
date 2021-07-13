53423934efa0da9f3465dc591b1b0c5d395fca43
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-07-12 17:31:02 -0700

    4

--- send_tokens.py
+++ send_tokens.py
@@ -3,21 +3,23 @@
 from algosdk.v2client import algod
 from algosdk import mnemonic
 from algosdk import transaction
+from algosdk.future.transaction import PaymentTxn
 from algosdk import account
 
-#Connect to Algorand node maintained by PureStake
-#Connect to Algorand node maintained by PureStake
+# Connect to Algorand node maintained by PureStake
+# Connect to Algorand node maintained by PureStake
 algod_address = "https://testnet-algorand.api.purestake.io/ps2"
 algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
-#algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
+# algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
 headers = {
-   "X-API-Key": algod_token,
+    "X-API-Key": algod_token,
 }
 
 acl = algod.AlgodClient(algod_token, algod_address, headers)
-min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
+min_balance = 100000  # https://developer.algorand.org/docs/features/accounts/#minimum-balance
 
-def send_tokens( receiver_pk, tx_amount ):
+
+def send_tokens(receiver_pk, tx_amount):
     params = acl.suggested_params()
     gen_hash = params.gh
     first_valid_round = params.first
@@ -27,14 +29,18 @@ def send_tokens( receiver_pk, tx_amount ):
     # generate an account
     private_key, account_address = account.generate_account()
     mnemonic_phrase = mnemonic.from_private_key(private_key)
-    public_key = mnemonic.to_public_key(mnemonic_phrase)
-    print("My mnemonic_phrase: {}".format(mnemonic_phrase))
-    print("My public_key: {}".format(public_key))
-    print("My account_address: {}".format(account_address))
+    # print("My mnemonic_phrase: {}".format(mnemonic_phrase))
+    # print("My account_address: {}".format(account_address))
 
+    # construct the transaction
+    unsigned_txn = transaction.PaymentTxn(account_address, tx_fee, first_valid_round, last_valid_round, gen_hash,
+                                          receiver_pk, tx_amount)
+    signed_txn = unsigned_txn.sign(mnemonic.to_private_key(mnemonic_phrase))
+    txid = acl.send_transaction(signed_txn)
+    print("Successfully sent transaction with txID: {}".format(txid))
 
+    return account_address, txid
 
-    return sender_pk, txid
 
 # Function from Algorand Inc.
 def wait_for_confirmation(client, txid):
@@ -51,4 +57,3 @@ def wait_for_confirmation(client, txid):
         txinfo = client.pending_transaction_info(txid)
     print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
     return txinfo
-
